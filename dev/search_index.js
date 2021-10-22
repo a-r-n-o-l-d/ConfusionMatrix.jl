9a514f1b1f238aa94c52448b7c134c53ae54ed4e
Documenter.jl <documenter@juliadocs.github.io>
2021-10-22 15:48:31 +0000

    build based on ff770e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConfusionMatrix","category":"page"},{"location":"#ConfusionMatrix","page":"Home","title":"ConfusionMatrix","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConfusionMatrix.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConfusionMatrix]","category":"page"}]
+}
